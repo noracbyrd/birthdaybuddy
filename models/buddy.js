00f6122faca2a5199b20efa58aa0201996eb1e90
noracyrd <norabyrd@alumni.upenn.edu>
2020-01-07 17:27:03 -0600

    tweaked models for mvp

--- models/buddy.js
+++ models/buddy.js
@@ -7,13 +7,14 @@ const buddySchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     date: {type: Date, default: Date.now},
+    // will need to make month two digits so it sorts properly
     month: {type: Number, required: true},
-    day: {type: Number, required: true},
-    year: {type: Number, required: false},
+    day: {type: Number, required: true}
     // in future versions: reminders. 
     // dayOfReminder: {type: Boolean, required: true},
     // dayBeforeReminder: {type: Boolean, required: true},
     // weekBeforeReminder: {type: Boolean, required: true},
+    //documents belonging to other connections:
 });
 
 const Buddy = mongoose.model("Buddy", buddySchema);
