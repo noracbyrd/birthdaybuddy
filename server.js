bdb0bb37783604d241aabd995ce04428f028259f
noracyrd <norabyrd@alumni.upenn.edu>
2020-01-07 17:51:23 -0600

    added connection file, started building out session & database connection

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8022;
 const path = require("path");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dbConnection = require("./connection");
+const session = require("express-session");
 
 // middleware
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// alternate middleware: should i be using this?
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: false
+//     })
+// )
+// app.use(bodyParser.json())
+
+//Mongoose DB Connection
+mongoose.Promise = global.Promise
 
 if (process.env.NODE_ENV === "production") {
  app.use(express.static("client/build"));
