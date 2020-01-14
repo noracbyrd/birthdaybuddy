1f2f55d92c6b45bcc7132404c619352dc5a1f0a8
noracyrd <norabyrd@alumni.upenn.edu>
2020-01-13 23:14:13 -0600

    building out sign up form

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -3,25 +3,49 @@ import React, { Component } from 'react'
 class Signup extends Component {
 
     state = {
-
+        username: "",
+        password: ""
+    }
+    handleInputChange = e => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+    handleFormSubmit = e => {
+        e.preventDefault()
+        const newUser = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        console.log(newUser)
+        //API.createUser(newUser) goes here
+        this.setState({
+            username: "",
+            password: ""
+        })
     }
     render() {
         return (
             <div>
+                <form>
                 <div className="row">
                     <div className="input-field col s12">
-                        <div><input value="I am not editable" type="text" className="validate"/>
-                            <label for="username">Disabled</label>
+                        <div><input name="username" value={this.state.title} type="text" className="validate" onChange={this.handleInputChange}/>
+                            <label for="username">Username</label>
                         </div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col s12">
-                        <div><input id="password" type="password" className="validate"/>
+                        <div><input name="password" value={this.state.password} type="password" className="validate" onChange={this.handleInputChange}/>
                             <label for="password">Password</label>
                         </div>
                     </div>
                 </div>
+                <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.handleFormSubmit}>Submit<i class="material-icons right"></i>
+                </button>
+                </form>
             </div>
         );
     }
