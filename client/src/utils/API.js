ad0cd4cd4c5d6e3a5018839feb7acb9474aa6581
noracyrd <norabyrd@alumni.upenn.edu>
2020-01-15 15:10:14 -0600

    built basic user front end api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,14 @@
 import axios from "axios"
 
 export default {
-    
+   // user routing
+   userLogin: function(loginData){
+       return axios.post("/api/users/login", loginData)
+   },
+   userLogout: function() {
+       return axios.post("/api/users/logout")
+   },
+   userSignup: function(newUser){
+       return axios.post("/api/users", newUser)
+   }
 }
